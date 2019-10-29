e31e7d090adb7e7b81c205da53ca29bb3e07dbae
Seitz, S. (BNotK) <s.seitz@bnotk.de>
2019-10-29 14:50:02 +0100

    added git_to_name()

--- src/modules/extension/resolve2/expr-xm_resolve2-funcproc.c
+++ src/modules/extension/resolve2/expr-xm_resolve2-funcproc.c
@@ -4,6 +4,20 @@
 
 /* FUNCTIONS */
 
+// gid_to_name
+const char *nx_expr_func__xm_resolve2_gid_to_name_integer_argnames[] = {
+    "gid", 
+};
+nx_value_type_t nx_expr_func__xm_resolve2_gid_to_name_integer_argtypes[] = {
+    NX_VALUE_TYPE_INTEGER, 
+};
+// gid_to_name
+const char *nx_expr_func__xm_resolve2_gid_to_name_string_argnames[] = {
+    "gid", 
+};
+nx_value_type_t nx_expr_func__xm_resolve2_gid_to_name_string_argtypes[] = {
+    NX_VALUE_TYPE_STRING, 
+};
 // ipaddr_to_name
 const char *nx_expr_func__xm_resolve2_ipaddr_to_name_string_argnames[] = {
     "ipaddr", 
@@ -13,6 +27,28 @@ nx_value_type_t nx_expr_func__xm_resolve2_ipaddr_to_name_string_argtypes[] = {
 };
 
 nx_expr_func_t nx_api_declarations_xm_resolve2_funcs[] = {
+ {
+   { .next = NULL, .prev = NULL },
+   NULL,
+   "gid_to_name",
+   NX_EXPR_FUNCPROC_TYPE_GLOBAL,
+   nx_expr_func__xm_resolve2_gid_to_name,
+   NX_VALUE_TYPE_STRING,
+   1,
+   nx_expr_func__xm_resolve2_gid_to_name_integer_argnames,
+   nx_expr_func__xm_resolve2_gid_to_name_integer_argtypes,
+ },
+ {
+   { .next = NULL, .prev = NULL },
+   NULL,
+   "gid_to_name",
+   NX_EXPR_FUNCPROC_TYPE_GLOBAL,
+   nx_expr_func__xm_resolve2_gid_to_name,
+   NX_VALUE_TYPE_STRING,
+   1,
+   nx_expr_func__xm_resolve2_gid_to_name_string_argnames,
+   nx_expr_func__xm_resolve2_gid_to_name_string_argtypes,
+ },
  {
    { .next = NULL, .prev = NULL },
    NULL,
@@ -27,7 +63,7 @@ nx_expr_func_t nx_api_declarations_xm_resolve2_funcs[] = {
 };
 
 nx_module_exports_t nx_module_exports_xm_resolve2 = {
-	1,
+	3,
 	nx_api_declarations_xm_resolve2_funcs,
 	0,
 	NULL,
